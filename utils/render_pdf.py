e21b144c3bface3921d6981144390dd652b9f026
Gonzalo Hernandez <stkierke@gmail.com>
2023-10-23 01:22:49 -0300

    Refactor

--- utils/render_pdf.py
+++ utils/render_pdf.py
@@ -1,7 +1,6 @@
 import os
 import base64
 import numpy as np
-import streamlit as st
 from docx2pdf import convert
 from datetime import datetime
 from docxtpl import DocxTemplate
@@ -42,5 +41,7 @@ def get_context(df):
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    pdf_display = f'''<iframe src="data:application/pdf;base64,{base64_pdf}"
+                        width="800" height="800" type="application/pdf">
+                      </iframe>'''
     return pdf_display
